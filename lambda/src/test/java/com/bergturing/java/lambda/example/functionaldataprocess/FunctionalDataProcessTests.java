bf831750fa534d9b42f7aea2ae99f9e99c3e2aba
berg-turing <993450432@qq.com>
2019-01-24 00:06:22 +0800

    增加java 8相关特性的测试代码

--- lambda/src/test/java/com/bergturing/java/lambda/example/functionaldataprocess/FunctionalDataProcessTests.java
+++ lambda/src/test/java/com/bergturing/java/lambda/example/functionaldataprocess/FunctionalDataProcessTests.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.toList;
